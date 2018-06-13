568643af43ce481d2030903d45c6f47f6f64c59a
Andy en lap <andreswong5@gmail.com>
2018-06-12 18:57:01 -0700

    hotfix dialer james

--- resources/views/telefono.blade.php
+++ resources/views/telefono.blade.php
@@ -1,55 +1,37 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="top1">
-    <h1>PSTN calling</h1>
-</div>
+
 
 <div class="container">
+    <div class="row" style="padding-top: 35px">
 
-    <div id="chromeFileWarning" class="frame big">
-        <b style="color: red;">Warning!</b> Protocol "file" used to load page in Chrome.<br><br>
-        Please avoid loading files directly from disk when developing WebRTC applications using Chrome.<br>
-        Chrome disables access to microphone which prevents proper functionality.<br>
-        <br>
-        You can allow working with "file:", if you start Chrome with the flag <i>--allow-file-access-from-files</i>
-    </div>
 
-    <div class="clearfix"></div>
 
-    <div class="frame small">
-        <div class="inner loginBox">
-            <h3 id="login">Sign in</h3>
+
+
+
+
+
+
+
             <form id="userForm">
-                <input id="username" placeholder="USERNAME"><br>
-                <input id="password" type="password" placeholder="PASSWORD"><br>
-                <button id="loginUser">Login</button>
-                <button id="createUser">Create</button>
+                <input id="username" type="hidden" placeholder="USERNAME" value="andreswong5@gmail.com"><br>
+                <input id="password" type="hidden" placeholder="PASSWORD" value="tito1212"><br>
+                <button id="loginUser">Iniciar</button>
+
             </form>
-            <div id="userInfo">
-                <h3><span id="username"></span></h3>
-                <button id="logOut">Logout</button>
-            </div>
-        </div>
 
-        <div class="inner takeaways">
-            <h3>Takeaways</h3>
-            <ul>
-                <li>Authenticate users and act on success / failures</li>
-                <li>How to create user and login automatically</li>
-                <li>After page load, look for an earlier session and try to start it</li>
-                <li>Place a PSTN call</li>
-                <li>Wire up the incoming stream + start/stop ringback tone as needed</li>
-                <li>Handle end of phone call</li>
-            </ul>
-        </div>
-    </div>
 
-    <div class="frame">
-        <h3>PSTN Call</h3>
+
+
+
+
+        <div class="card card-header">Telefono</div>
+        <div class="card card-body">
         <div id="call">
             <form id="newCall">
-                <input id="phoneNumber" placeholder="Phone Number (+46000000000)"><br>
+                <input id="phoneNumber" placeholder="Numero de telefono +1 714 760 9878"><br>
                 <button id="call">Call</button>
                 <button id="hangup">Hangup</button>
                 <audio id="incoming" autoplay></audio>
@@ -61,6 +43,8 @@
         </div>
         <div class="error">
         </div>
-    </div>
+        </div>
+
 </div>
+
     @endsection
