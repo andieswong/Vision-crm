05545f46874935d19b2ba7a65d274262f1ef1942
Andres wong <andreswong5@gmail.com>
2019-04-22 23:46:26 -0700

    Automarcado completo

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,20 +10,20 @@
 
     <title>Vision.</title>
 
-        <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
-        <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:300' rel='stylesheet' type='text/css'>
-        <link rel="dns-prefetch" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto:100,300,400|Slabo+27px" rel="stylesheet">
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}" rel="stylesheet">
-        <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
-              integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
-              crossorigin="anonymous">
-        <link rel="stylesheet" href="{{ mix("css/app.css") }}">
+    <link href='https://fonts.googleapis.com/css?family=Roboto:400,300' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed:300' rel='stylesheet' type='text/css'>
+    <link rel="dns-prefetch" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,600" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto:100,300,400|Slabo+27px" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet"
+          integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN"
+          crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ mix("css/app.css") }}">
     <link rel="stylesheet" href="{{ asset("css/site.css") }}">
-        <script src="{{ asset('js/twilio.js') }}" defer></script>
-        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <script type="text/javascript" src="https://media.twiliocdn.com/sdk/js/client/v1.7/twilio.min.js"></script>
+    <script src="{{ asset('js/twilio.js') }}" defer></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script type="text/javascript" src="https://media.twiliocdn.com/sdk/js/client/v1.7/twilio.min.js"></script>
 
 
 </head>
@@ -61,6 +61,7 @@
                                 <li class="nav"><a class="nav-link" href="/Leads" style="color: #ffffff;">Leads</a></li>
                                 <li class="nav"><a class="nav-link" href="/Contactos"
                                                    style="color: #ffffff;">Contactos</a></li>
+                                <li class="nav"><a class="nav-link" href="/Call">Dialer</a></li>
                                 {{--                                <li class="nav"><a class="nav-link" href="/Prefijos"--}}
                                 {{--                                                   style="color: #ffffff;">Prefijos</a></li>--}}
                                 <li class="nav"><a class="nav-link" href="/Agentes" style="color: #ffffff;">Agentes</a>
