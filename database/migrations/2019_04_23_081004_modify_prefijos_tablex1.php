dd08561415975e4dbd9951dcb7a6a268b5def97f
Andres wong <andreswong5@gmail.com>
2019-04-23 04:43:16 -0700

    hotfix

--- database/migrations/2019_04_23_081004_modify_prefijos_tablex1.php
+++ database/migrations/2019_04_23_081004_modify_prefijos_tablex1.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ModifyPrefijosTable extends Migration
+class ModifyPrefijosTablex1 extends Migration
 {
     /**
      * Run the migrations.
