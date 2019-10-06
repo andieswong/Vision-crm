3d51ef19faceb8b36384974c5820a5fd1b32c5da
Andres wong <andreswong5@gmail.com>
2019-10-05 17:31:49 -0700

    fix mix content

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,9 +15,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if (env('APP_ENV') === 'production') {
-          URL::forceSchema('https');
-        }
+
     }
 
     /**
